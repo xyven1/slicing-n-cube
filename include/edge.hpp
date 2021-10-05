9b9f9f52e2929c10d7d4d3cc7ed9ac4fe3a490e4
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2021-10-05 13:29:27 +0200

    Add edge file

---
+++ include/edge.hpp
@@ -0,0 +1,19 @@
+#ifndef EDGE_H
+#define EDGE_H
+
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
+#include "common.hpp"
+
+inline int32_t edge_to_int(const edge_t& e, const std::vector<edge_t>& edges) {
+  const auto e_it = std::lower_bound(edges.begin(), edges.end(), e);
+  return static_cast<int32_t>(e_it - edges.begin());
+}
+
+inline int32_t num_edges(int32_t n) { return 2 * pow(2, n - 1); }
+
+std::vector<edge_t> compute_edges(int32_t);
+
+#endif
---
+++ src/edge.cpp
@@ -0,0 +1,20 @@
+#include "edge.hpp"
+
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
+std::vector<edge_t> compute_edges(int32_t n) {
+  std::vector<edge_t> edges;
+  edges.reserve(num_edges(n));
+  for (vertex_t v = 0; v < (1u << n); ++v) {
+    for (int32_t i = 0; i < n; ++i) {
+      const vertex_t neighbour = v ^ (1 << i);
+      if (v < neighbour) {
+        edges.emplace_back(v, neighbour);
+      }
+    }
+  }
+  std::sort(edges.begin(), edges.end());
+  return edges;
+}
