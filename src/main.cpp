31c9f54d68bdb653462b313d5e90627443dd2eb8
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2021-10-22 12:55:24 +0200

    Print some interesting stats

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,37 @@ void write_two_sliceable_sets(int32_t n) {
   write_to_file(mss_2, NCUBE_DIR + std::to_string(n) + "_mss_2.bin");
 }
 
+void print_sizes(int32_t n) {
+  const std::string usr_path_1 = NCUBE_DIR + std::to_string(n) + "_usr_1.bin";
+  const std::string mss_path_1 = NCUBE_DIR + std::to_string(n) + "_mss_1.bin";
+  const std::string usr_path_2 = NCUBE_DIR + std::to_string(n) + "_usr_2.bin";
+  const std::string mss_path_2 = NCUBE_DIR + std::to_string(n) + "_mss_2.bin";
+  const std::vector<sliceable_set_t> usr_1 = read_from_file(usr_path_1);
+  const std::vector<sliceable_set_t> mss_1 = read_from_file(mss_path_1);
+  const std::vector<sliceable_set_t> usr_2 = read_from_file(usr_path_2);
+  const std::vector<sliceable_set_t> mss_2 = read_from_file(mss_path_2);
+  std::cout << "n = " << n << std::endl;
+  std::cout << "usr_1 size = " << usr_1.size() << std::endl;
+  std::cout << "mss_1 size = " << mss_1.size() << std::endl;
+  std::cout << "usr_2 size = " << usr_2.size() << std::endl;
+  std::cout << "mss_2 size = " << mss_2.size() << std::endl;
+}
+
+std::vector<int64_t> compute_edge_frequencies(int32_t n) {
+  const std::vector<vertex_trans_t> vertex_transformations =
+      compute_vertex_transformations(n);
+  const std::vector<complex_t> complexes =
+      compute_cut_complexes(vertex_transformations, n);
+  const std::vector<edge_t> edges = compute_edges(n);
+  const std::vector<sliceable_set_t> mss =
+      complexes_to_mss(complexes, vertex_transformations, edges, n);
+  std::vector<int64_t> frequencies(num_edges(n) + 1);
+  for (const sliceable_set_t& ss : mss) {
+    frequencies[ss.count()] += 1;
+  }
+  return frequencies;
+}
+
 int main() {
   constexpr int32_t n = N;
   const std::string usr_path = NCUBE_DIR + std::to_string(n) + "_usr_2.bin";
