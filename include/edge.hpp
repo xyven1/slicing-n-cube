473d8ba1bc55f617191241d5053d03367a5a6aa9
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2021-11-27 17:44:26 +0100

    Make edge and symmetry header-only

--- include/edge.hpp
+++ include/edge.hpp
@@ -12,6 +12,19 @@ inline int32_t edge_to_int(const edge_t& e, const std::vector<edge_t>& edges) {
   return static_cast<int32_t>(e_it - edges.begin());
 }
 
-std::vector<edge_t> compute_edges(int32_t n);
+std::vector<edge_t> compute_edges(int32_t n) {
+  std::vector<edge_t> edges;
+  edges.reserve(num_edges(n));
+  for (vertex_t v = 0; v < num_vertices(n); ++v) {
+    for (int32_t i = 0; i < n; ++i) {
+      const vertex_t neighbour = v ^ (1 << i);
+      if (v < neighbour) {
+        edges.emplace_back(v, neighbour);
+      }
+    }
+  }
+  std::sort(edges.begin(), edges.end());
+  return edges;
+}
 
 #endif
--- include/symmetry.hpp
+++ include/symmetry.hpp
@@ -1,21 +1,89 @@
 #ifndef SYMMETRY_H
 #define SYMMETRY_H
 
+#include <algorithm>
 #include <cstdint>
 #include <vector>
 
 #include "common.hpp"
+#include "edge.hpp"
 
-std::vector<symmetry_t> compute_symmetries(int32_t n);
+std::vector<symmetry_t> compute_symmetries(int32_t n) {
+  std::vector<symmetry_t> symmetries;
+  symmetries.reserve(num_symmetries(n));
 
-vertex_t transform_vertex(const symmetry_t& sym, vertex_t v, int32_t n);
+  std::vector<int32_t> permutation(n);
+  for (int32_t i = 0; i < n; ++i) {
+    permutation[i] = i;
+  }
+  do {
+    // if the i-th least significant bit is set, negate the i-th coordinate
+    for (uint32_t signs = 0; signs < (1u << n); ++signs) {
+      symmetry_t symmetry;
+      symmetry.reserve(n);
+      for (int32_t i = 0; i < n; ++i) {
+        const uint32_t sign = (signs >> i) & 1;
+        const uint32_t sign_and_position = (sign << 31) | permutation[i];
+        symmetry.push_back(sign_and_position);
+      }
+      symmetries.push_back(symmetry);
+    }
+  } while (std::next_permutation(permutation.begin(), permutation.end()));
+  return symmetries;
+}
 
-vertex_t transform_vertex_inv(const symmetry_t& sym, vertex_t v, int32_t n);
+vertex_t transform_vertex(const symmetry_t& sym, vertex_t v, int32_t n) {
+  vertex_t transformation = 0;
+  for (int32_t i = 0; i < n; ++i) {
+    const uint32_t sign = (sym[i] >> 31) & 1;
+    const uint32_t position = sym[i] & 0x7FFFFFFF;
+    const uint32_t val_i = (v >> i) & 1;
+    const uint32_t val_i_sign = val_i ^ sign;
+    const uint32_t val_i_sign_and_position = val_i_sign << position;
+    transformation |= val_i_sign_and_position;
+  }
+  return transformation;
+}
 
-std::vector<vertex_trans_t> compute_vertex_transformations(int32_t n);
+vertex_t transform_vertex_inv(const symmetry_t& sym, vertex_t v, int32_t n) {
+  vertex_t transformation = 0;
+  for (int32_t i = 0; i < n; ++i) {
+    const uint32_t sign = (sym[i] >> 31) & 1;
+    const uint32_t position = sym[i] & 0x7FFFFFFF;
+    const uint32_t val_i = (v >> position) & 1;
+    const uint32_t val_i_sign = val_i ^ sign;
+    const uint32_t val_i_sign_and_position = val_i_sign << i;
+    transformation |= val_i_sign_and_position;
+  }
+  return transformation;
+}
+
+std::vector<vertex_trans_t> compute_vertex_transformations(int32_t n) {
+  const std::vector<symmetry_t> symmetries = compute_symmetries(n);
+  std::vector<vertex_trans_t> transformations(symmetries.size());
+  for (std::size_t i = 0; i < symmetries.size(); ++i) {
+    transformations[i].reserve(num_vertices(n));
+    for (vertex_t v = 0; v < num_vertices(n); ++v) {
+      transformations[i].push_back(transform_vertex_inv(symmetries[i], v, n));
+    }
+  }
+  return transformations;
+}
 
 std::vector<edge_trans_t> compute_edge_transformations(
     const std::vector<edge_t>& edges,
-    const std::vector<vertex_trans_t>& vertex_transformations, int32_t n);
+    const std::vector<vertex_trans_t>& vertex_transformations, int32_t n) {
+  std::vector<edge_trans_t> edge_transformations(vertex_transformations.size());
+  for (std::size_t i = 0; i < edge_transformations.size(); ++i) {
+    edge_transformations[i].reserve(num_edges(n));
+    for (int32_t e = 0; e < num_edges(n); ++e) {
+      const vertex_t u = vertex_transformations[i][edges[e].first];
+      const vertex_t v = vertex_transformations[i][edges[e].second];
+      const edge_t e_inversion = (u < v) ? edge_t(u, v) : edge_t(v, u);
+      edge_transformations[i].push_back(edge_to_int(e_inversion, edges));
+    }
+  }
+  return edge_transformations;
+}
 
 #endif
