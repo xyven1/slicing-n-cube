90b2de5adbc98777f94ea88504745abebd6e03ef
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2021-12-30 22:26:59 +0100

    Compute 2-sliceable sets

--- src/slice_cube_degree_two.cpp
+++ src/slice_cube_degree_two.cpp
@@ -13,10 +13,12 @@ int main() {
   const auto edges = compute_edges(N);
   const auto edge_transformations =
       compute_edge_transformations(edges, vertex_transformations, N);
-  const auto usr = read_from_file<N>(NCUBE_DIR "degree_two/" +
-                                     std::to_string(N) + "_usr_1.bin");
-  const auto mss = read_from_file<N>(NCUBE_DIR "degree_two/" +
-                                     std::to_string(N) + "_usr_1.bin");
-  std::cout << usr.size() << std::endl;
-  std::cout << mss.size() << std::endl;
+  const auto usr_1 = read_from_file<N>(NCUBE_DIR "degree_two/" +
+                                       std::to_string(N) + "_usr_1.bin");
+  const auto mss_1 = read_from_file<N>(NCUBE_DIR "degree_two/" +
+                                       std::to_string(N) + "_mss_1.bin");
+  const auto usr_2 = combine_usr_mss<N>(usr_1, mss_1, edge_transformations);
+  std::cout << "|usr_2| = " << usr_2.size() << std::endl;
+  const auto mss_2 = usr_to_mss<N>(usr_2, edge_transformations);
+  std::cout << "|mss_2| = " << mss_2.size() << std::endl;
 }
