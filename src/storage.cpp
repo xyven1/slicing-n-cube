62d09898af5775047e578bba9b97e8f637bf388c
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2021-12-30 21:57:32 +0100

    Refactor output paths

--- src/storage.cpp
+++ src/storage.cpp
@@ -20,14 +20,13 @@ void write_degree_two_sliceable_sets() {
   auto mss = usr_to_mss<N>(usr, edge_transformations);
   std::sort(usr.begin(), usr.end());
   std::sort(mss.begin(), mss.end());
-  write_to_file<N>(usr,
-                   NCUBE_DIR "degree_two/" + std::to_string(N) + "_usr_1.bin");
-  write_to_file<N>(mss,
-                   NCUBE_DIR "degree_two/" + std::to_string(N) + "_mss_1.bin");
+  const auto directory = NCUBE_DIR "degree_two/";
+  write_to_file<N>(usr, directory + std::to_string(N) + "_usr_1.bin");
+  write_to_file<N>(mss, directory + std::to_string(N) + "_mss_1.bin");
 }
 
 template <int32_t N>
-void write_two_sliceable_sets() {
+void write_degree_one_sliceable_sets() {
   const auto vertex_transformations = compute_vertex_transformations(N);
   const auto complexes =
       compute_cut_complexes_degree_one<N>(vertex_transformations);
@@ -42,17 +41,18 @@ void write_two_sliceable_sets() {
   std::sort(mss.begin(), mss.end());
   std::sort(usr_2.begin(), usr_2.end());
   std::sort(mss_2.begin(), mss_2.end());
-  write_to_file<N>(usr, NCUBE_DIR + std::to_string(N) + "_usr_1.bin");
-  write_to_file<N>(mss, NCUBE_DIR + std::to_string(N) + "_mss_1.bin");
-  write_to_file<N>(usr_2, NCUBE_DIR + std::to_string(N) + "_usr_2.bin");
-  write_to_file<N>(mss_2, NCUBE_DIR + std::to_string(N) + "_mss_2.bin");
+  const auto directory = NCUBE_DIR "degree_one/";
+  write_to_file<N>(usr, directory + std::to_string(N) + "_usr_1.bin");
+  write_to_file<N>(mss, directory + std::to_string(N) + "_mss_1.bin");
+  write_to_file<N>(usr_2, directory + std::to_string(N) + "_usr_2.bin");
+  write_to_file<N>(mss_2, directory + std::to_string(N) + "_mss_2.bin");
 }
 
 int main() {
-  write_two_sliceable_sets<2>();
-  write_two_sliceable_sets<3>();
-  write_two_sliceable_sets<4>();
-  write_two_sliceable_sets<5>();
+  write_degree_one_sliceable_sets<2>();
+  write_degree_one_sliceable_sets<3>();
+  write_degree_one_sliceable_sets<4>();
+  write_degree_one_sliceable_sets<5>();
   write_degree_two_sliceable_sets<2>();
   write_degree_two_sliceable_sets<3>();
   write_degree_two_sliceable_sets<4>();
