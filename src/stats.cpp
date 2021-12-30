81de16ca2f33c021ae7f4a9132486aa8bc64fb69
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2021-12-30 18:17:04 +0100

    Embrace auto

--- src/stats.cpp
+++ src/stats.cpp
@@ -9,15 +9,14 @@
 
 template <int32_t N>
 std::vector<int64_t> compute_edge_frequencies() {
-  const std::vector<vertex_trans_t> vertex_transformations =
-      compute_vertex_transformations(N);
-  const std::vector<complex_t<N>> complexes =
+  const auto vertex_transformations = compute_vertex_transformations(N);
+  const auto complexes =
       compute_cut_complexes_degree_one<N>(vertex_transformations);
-  const std::vector<edge_t> edges = compute_edges(N);
-  const std::vector<sliceable_set_t<N>> mss =
+  const auto edges = compute_edges(N);
+  const auto mss =
       complexes_to_mss<N>(complexes, vertex_transformations, edges);
   std::vector<int64_t> frequencies(num_edges(N) + 1);
-  for (const sliceable_set_t<N>& ss : mss) {
+  for (const auto& ss : mss) {
     frequencies[ss.count()] += 1;
   }
   return frequencies;
@@ -26,13 +25,13 @@ std::vector<int64_t> compute_edge_frequencies() {
 template <int32_t N>
 int64_t count_pairs_cardinality(const std::vector<sliceable_set_t<N>>& usr,
                                 const std::vector<sliceable_set_t<N>>& mss) {
-  constexpr std::size_t max_cardinality = sliceable_set_t<N>().size();
+  constexpr auto max_cardinality = sliceable_set_t<N>().size();
   std::vector<int64_t> cardinalities(max_cardinality + 1);
-  for (const sliceable_set_t<N>& ss : mss) {
+  for (const auto& ss : mss) {
     cardinalities[ss.count()] += 1;
   }
   int64_t count = 0;
-  for (const sliceable_set_t<N>& ss : usr) {
+  for (const auto& ss : usr) {
     for (std::size_t i = max_cardinality - ss.count(); i < cardinalities.size();
          ++i) {
       count += cardinalities[i];
@@ -45,11 +44,11 @@ template <int32_t N>
 int64_t count_pairs_leading_zeros(const std::vector<sliceable_set_t<N>>& usr,
                                   const std::vector<sliceable_set_t<N>>& mss) {
   std::vector<int64_t> leading_ones(sliceable_set_t<N>().size() + 1);
-  for (const sliceable_set_t<N>& ss : mss) {
+  for (const auto& ss : mss) {
     leading_ones[get_leading_ones<N>(ss)] += 1;
   }
   int64_t count = 0;
-  for (const sliceable_set_t<N>& ss : usr) {
+  for (const auto& ss : usr) {
     for (std::size_t i = get_leading_zeros<N>(ss); i < leading_ones.size();
          ++i) {
       count += leading_ones[i];
