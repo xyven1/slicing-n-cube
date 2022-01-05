bce045d1a71c7a92112124000e3a7d783ff4553b
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-01-05 15:20:39 +0100

    Print progress

--- src/slice_cube.cpp
+++ src/slice_cube.cpp
@@ -7,15 +7,20 @@
 #include "symmetry.hpp"
 #include "vertex.hpp"
 
-bool slice_cube_5() {
+void slice_cube_5() {
   constexpr int32_t N = 5;
   const auto edges = compute_edges(N);
   const auto complexes = compute_cut_complexes_degree_one<N>();
   const auto usr_1 = complexes_to_usr<N>(complexes, edges);
+  std::cout << usr_1.size() << std::endl;
   const auto mss_1 = usr_to_mss<N>(usr_1, edges);
+  std::cout << mss_1.size() << std::endl;
   const auto usr_2 = pairwise_unions<N>(usr_1, mss_1, edges);
+  std::cout << usr_2.size() << std::endl;
   const auto mss_2 = usr_to_mss<N>(usr_2, edges);
-  return pairwise_unions_slice_cube<N>(usr_2, mss_2);
+  std::cout << mss_2.size() << std::endl;
+  const auto slices_cube = pairwise_unions_slice_cube<N>(usr_2, mss_2);
+  std::cout << slices_cube << std::endl;
 }
 
-int main() { std::cout << slice_cube_5() << std::endl; }
+int main() { slice_cube_5(); }
