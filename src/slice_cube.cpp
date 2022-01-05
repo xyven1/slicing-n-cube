b6ba59438c61b4b11c98566517163bc22f410ef0
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-01-05 13:37:38 +0100

    Fix slice_cube

--- src/slice_cube.cpp
+++ src/slice_cube.cpp
@@ -7,15 +7,15 @@
 #include "symmetry.hpp"
 #include "vertex.hpp"
 
-template <int32_t N>
-bool slice_cube() {
+bool slice_cube_5() {
+  constexpr int32_t N = 5;
   const auto edges = compute_edges(N);
   const auto complexes = compute_cut_complexes_degree_one<N>();
-  const auto usr = complexes_to_usr<N>(complexes, edges);
-  const auto mss = usr_to_mss<N>(usr, edges);
-  const auto usr_2 = combine_usr_mss<N>(usr, mss, edges);
+  const auto usr_1 = complexes_to_usr<N>(complexes, edges);
+  const auto mss_1 = usr_to_mss<N>(usr_1, edges);
+  const auto usr_2 = combine_usr_mss<N>(usr_1, mss_1, edges);
   const auto mss_2 = usr_to_mss<N>(usr_2, edges);
-  return combine_usr_mss_final<N>(usr, mss);
+  return combine_usr_mss_final<N>(usr_2, mss_2);
 }
 
-int main() { std::cout << slice_cube<5>() << std::endl; }
+int main() { std::cout << slice_cube_5() << std::endl; }
