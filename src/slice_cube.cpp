3ecc0625869e2a285ae7a862348fcaeedf306382
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-01-06 21:30:16 +0100

    Slice 5-cube with 4 hyperplanes

--- src/slice_cube.cpp
+++ src/slice_cube.cpp
@@ -8,20 +8,19 @@
 
 using namespace ncube;
 
-void slice_cube_5() {
+bool slice_5_cube() {
   constexpr int32_t N = 5;
   const auto edges = compute_edges<N>();
   const auto complexes = compute_complexes<N>(is_complex_degree_one<N>);
   const auto usr_1 = complexes_to_usr<N>(complexes, edges);
-  std::cout << usr_1.size() << std::endl;
   const auto mss_1 = expand_usr<N>(usr_1, edges);
-  std::cout << mss_1.size() << std::endl;
   const auto usr_2 = pairwise_unions<N>(usr_1, mss_1, edges);
-  std::cout << usr_2.size() << std::endl;
   const auto mss_2 = expand_usr<N>(usr_2, edges);
-  std::cout << mss_2.size() << std::endl;
   const auto slices_cube = pairwise_unions_slice_cube<N>(usr_2, mss_2);
-  std::cout << slices_cube << std::endl;
+  return slices_cube;
 }
 
-int main() { slice_cube_5(); }
+int main() {
+  std::cout << "Can four hyperplanes slice the 5-cube: " << slice_5_cube()
+            << std::endl;
+}
