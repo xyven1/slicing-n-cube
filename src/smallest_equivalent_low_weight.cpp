e773cc2f29e8286a413741731838844012802e16
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-01-13 13:18:02 +0100

    More output

--- src/smallest_equivalent_low_weight.cpp
+++ src/smallest_equivalent_low_weight.cpp
@@ -11,51 +11,61 @@
 using namespace ncube;
 
 /**
- *  Returns the smallest i so that low weight halfspaces whose normal vector
+ *  Outputs the smallest i so that low weight halfspaces whose normal vector
  *  contains only values in {-i, ..., i} have the same maximal sliceable sets as
  *  in the general setting.
  **/
 template <int32_t N>
-int32_t equivalent_low_weight_mss() {
+void equivalent_low_weight_mss() {
+  std::cout << "n = " << N << std::endl;
   const auto complexes = compute_complexes<N>(is_complex_degree_one<N>);
   const auto edges = compute_edges<N>();
   const auto usr = complexes_to_usr<N>(complexes, edges);
   const auto mss = expand_usr<N>(usr, edges);
-  for (int i = 0;; ++i) {
+  std::cout << "  |mss| = " << mss.size() << std::endl;
+  for (int i = 1;; ++i) {
     auto mss_low_weight = compute_low_weight_mss<N>(i, edges);
     std::sort(mss_low_weight.begin(), mss_low_weight.end());
+    std::cout << "  |mss_" << i << "| = " << mss_low_weight.size() << std::endl;
     if (mss_low_weight == mss) {
-      return i;
+      std::cout << "  smallest i to have equivalent mss is " << i << std::endl;
+      return;
     }
   }
 }
 
 /**
- *  Returns the smallest i so that low weight halfspaces whose normal vector
+ *  Outputs the smallest i so that low weight halfspaces whose normal vector
  *  contains only values in {-i, ..., i} slice the n-cube with the same number
  *  of sliceable sets as in the general setting.
  **/
 template <int32_t N>
-int32_t equivalent_low_weight_slice_cube_min() {
+void equivalent_low_weight_slice_cube_min() {
+  std::cout << "n = " << N << std::endl;
   const auto complexes = compute_complexes<N>(is_complex_degree_one<N>);
   const auto edges = compute_edges<N>();
   const auto usr = complexes_to_usr<N>(complexes, edges);
   const auto k = slice_cube_min<N>(usr, edges);
-  for (int i = 0;; ++i) {
+  std::cout << "  k = " << k << std::endl;
+  for (int i = 1;; ++i) {
     const auto mss_low_weight = compute_low_weight_mss<N>(i, edges);
     const auto usr_low_weight = reduce_to_usr<N>(mss_low_weight, edges);
     const auto k_low_weight = slice_cube_min<N>(usr_low_weight, edges);
+    std::cout << "  k_" << i << " = " << k << std::endl;
     if (k == k_low_weight) {
-      return i;
+      std::cout << "  smallest i to slice the n-cube with the same number of "
+                   "hyperplanes is "
+                << i << std::endl;
+      return;
     }
   }
 }
 
 int main() {
-  std::cout << "n=3 " << equivalent_low_weight_mss<3>() << std::endl;
-  std::cout << "n=4 " << equivalent_low_weight_mss<4>() << std::endl;
-  std::cout << "n=5 " << equivalent_low_weight_mss<5>() << std::endl;
-  std::cout << "n=3 " << equivalent_low_weight_slice_cube_min<3>() << std::endl;
-  std::cout << "n=4 " << equivalent_low_weight_slice_cube_min<4>() << std::endl;
-  std::cout << "n=5 " << equivalent_low_weight_slice_cube_min<5>() << std::endl;
+  equivalent_low_weight_mss<3>();
+  equivalent_low_weight_mss<4>();
+  equivalent_low_weight_mss<5>();
+  equivalent_low_weight_slice_cube_min<3>();
+  equivalent_low_weight_slice_cube_min<4>();
+  equivalent_low_weight_slice_cube_min<5>();
 }
