9cda7c88b7ddd7c5e8e4ea196fdd121ca8bac8b6
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-01-13 16:03:17 +0100

    C++ implementation of slice_cube_final matches C

--- src/slice_cube.cpp
+++ src/slice_cube.cpp
@@ -1,34 +1,22 @@
 #include <chrono>
-#include <cstdint>
 #include <iostream>
 
-#include "complex.hpp"
-#include "edge.hpp"
 #include "sliceable_set.hpp"
-#include "vertex.hpp"
 
 using namespace ncube;
 
-bool slice_5_cube() {
-  constexpr int32_t N = 5;
-  const auto edges = compute_edges<N>();
-  const auto complexes = compute_complexes<N>(is_complex_degree_one<N>);
-  const auto usr_1 = complexes_to_usr<N>(complexes, edges);
-  const auto mss_1 = expand_usr<N>(usr_1, edges);
-  const auto usr_2 = pairwise_unions<N>(usr_1, mss_1, edges);
-  const auto mss_2 = expand_usr<N>(usr_2, edges);
+int main() {
+  constexpr auto usr_2_path = N_CUBE_OUT_DIR "/degree_one/5_usr_2.bin";
+  constexpr auto mss_2_path = N_CUBE_OUT_DIR "/degree_one/5_mss_2.bin";
+  const auto usr_2 = read_from_file<5>(usr_2_path);
+  const auto mss_2 = read_from_file<5>(mss_2_path);
   const auto start = std::chrono::high_resolution_clock::now();
-  const auto slices_cube = pairwise_unions_slice_cube<N>(usr_2, mss_2);
+  const auto slices_cube = pairwise_unions_slice_cube<5>(usr_2, mss_2);
   const auto stop = std::chrono::high_resolution_clock::now();
   const auto duration =
       std::chrono::duration_cast<std::chrono::seconds>(stop - start);
   std::cout << "Execution time of pairwise_unions_slice_cube: "
             << duration.count() << std::endl;
-  return slices_cube;
-}
-
-int main() {
-  const auto slices_all = slice_5_cube();
-  std::cout << "Can four hyperplanes slice the 5-cube: " << slices_all
+  std::cout << "Can four hyperplanes slice the 5-cube: " << slices_cube
             << std::endl;
 }
